3024c5cfa39b766aac54450bfe602dbcfc0d7f8f
Nisha282/training <sahunishaji1220@gmail.com>
2022-08-16 17:50:24 +0530

    something

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -2,15 +2,31 @@ const mongoose = require('mongoose');
 
 
 const bookSchema = new mongoose.Schema( {
-    bookName : String ,
-    authorName : String,
-    category : {
-       type :String ,
-       enum :[  "horror", "fantasy", "graphic novel"]
-    },
-    year : Number
-
+  bookName: String, 
+  authorName: String, 
+  tags: [String],
+  
+  isPublished: Boolean,
+  prices: {
+      indianPrice: String,
+      europePrice: String,
   },
-{timestamps: true });
+  sales: {type: Number, default: 10}
+}, { timestamps: true });
 
-module.exports = mongoose.model('Book' ,bookSchema )
+
+module.exports = mongoose.model('Book', bookSchema) //users
+
+//Validation:
+//require:true
+//unique
+// default
+
+//String
+//Number
+//Date
+//Boolean
+// Arrays
+// Object
+// ObjectId
+// Buffer - not cover
